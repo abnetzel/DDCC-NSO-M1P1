0bb6114cc3ff1f01700014e5fc8be53cfe07a9bc
abnetzel <38819089+abnetzel@users.noreply.github.com>
2024-10-19 17:06:57 -0400

    10/19/2024

---
+++ sclb_extras/activity_info.js
@@ -0,0 +1,26 @@
+/**
+ *  ==================================================
+ *  SoftChalk LessonBuilder
+ *  Activity activity_info.js
+ *  Copyright 2002-2019 SoftChalk LLC
+ *  All Rights Reserved.
+ *
+ *  http://www.softchalk.com
+ *  ==================================================
+ */
+
+q_done_a=[];
+scoreQa=[];
+score_a=[];
+show_restart_a=[];
+num_pages=7;
+actOrder=[];
+actOrder[0]=4;
+
+//a_num=4;
+a_value4=0;
+scoreQa[4]=false;
+q_done_a[4]=false;
+score_a[4]=0;
+show_restart_a[4]=true;
+
---
+++ sclb_extras/lesson_qp.js
@@ -0,0 +1,49 @@
+/**
+ *  SoftChalk LessonBuilder
+ *  ================ DO NOT EDIT ================
+ */
+
+q_item=2;
+group_item=0;
+tableToggle_array=[];
+f_done=[];
+feed=[];
+q_done=[];
+scoreQ=[];
+qOrder=[];
+qOrder[0]=2;
+num_pages=7;
+scorm_completed_status=true;
+
+//q_num=2;
+inline_feedback2=true;
+q_done[2]=false;
+q_value2=0;
+scoreQ[2]="no";
+mc_items2=2;
+//ra_items2=1;
+q_type2=1;
+theQuestion2="<p>Did you watch all of the videos in this lesson and write down your Holland Code?</p>";
+showCorrect2="no";
+showHint2="no";
+showFeedbackIndividual2="yes";
+case_sensitive2="false";
+partial_credit2=false;
+feedbackRight2="<p>Great! You're ready to move on!</p>";
+feedbackWrong2="<p>Sorry, incorrect answer. Before proceeding, you will need to go back and watch the videos. You'll need to know your Holland Code and how to use Virtual Job Shadow before proceeding.</p>";
+feedbackPartial2="Partially correct.";
+hint2="";
+feedbackIndividual2=[];
+feedbackIndividual2[0]="";
+feedbackIndividual2[1]="";
+right_answers2=[];
+right_answers2[0]="a";
+answer_choices2=[];
+answer_choices2[0]="<p>Yes, I watched all of the videos and followed along with them. I know my Holland Code and understand how to use Virtual Job Shadow.</p>";
+answer_choices2[1]="<p>No, I did not watch all of the videos. I do not know my Holland Code, and I'm not sure how to use Virtual Job Shadow.</p>";
+tableToggle_array[2]=false;
+f_done[2]=false;
+feed[2]=false;
+//allowRetry2=true;
+//groupID2=0;
+
